ddd30f12c1473bd3e8b0d7f819e48e6c298cbe59
Dustin Starke and Hingyi Khong <hingyi@>
2018-08-13 16:17:15 -0700

    make luke awesome by showing us a clean way to append list element

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,15 @@ $(document).ready(function() {
   $("#icecreamflavors").submit(function(event) {
     event.preventDefault();
 
-    var flavors = ["Chocolate", "HazelNut", "ButterPecan", "PeanutButter"];
+    var flavors = ["Chocolate","HazelNut","ButterPecan","PeanutButter"];
     console.log(flavors);
     flavors.forEach(function(flavor) {
-        alert("I like " + flavor + ".");
+        $("#list").append("<li>" + flavor + "</li>");
+        // alert("I like " + flavor + ".");
 
       // console.log(flavors);
 
 });
-      // $("#list").append("<li>" + #[0] + "</li>" + "<li>" + #[1] + "</li>");
+
   });
 });
